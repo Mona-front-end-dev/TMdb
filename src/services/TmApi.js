df4763aa40fd7a15c60d04e1b3127495a7f94d17
Mona Khorasani <mona.khorasani@medieinsitutet.se>
2021-09-10 18:24:33 +0200

    Implemented API and component-the most popular movies

--- src/services/TmApi.js
+++ src/services/TmApi.js
@@ -13,11 +13,19 @@
    return respons;
  };
  
- //create andpoint to get latest released movies
+ //create Endpoint to get latest released movies
  export const getLatestReleased = async () => {
    const endpoint = 'discover/movie?api_key=e11ef120fb831c1ff10324b71ce6c686&language=en-US&page=1&region=US&sort_by=release_date.desc&release_date.lte=2021-09-08'; 
    const data = await get(endpoint);
    return data.data;
  };
 
+ //create Endpoint to get the most popular movies
+ export const getMostPopular = async () => {
+  const endpoint = 'discover/movie?api_key=e11ef120fb831c1ff10324b71ce6c686&language=en-US&page=1&region=US&primary_release_date.lte=2021-09-08'; 
+  const data = await get(endpoint);
+  return data.data;
+};
+
  
+
