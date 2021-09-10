3f8ebcbf25e0bc33eeda1e17c5bebbeef37b6bcd
Mona Khorasani <mona.khorasani@medieinsitutet.se>
2021-09-10 18:26:42 +0200

    Cleaning

--- src/components/Movies/Latest.jsx
+++ src/components/Movies/Latest.jsx
@@ -1,17 +1,14 @@
 import React from 'react';
 import Card from 'react-bootstrap/Card';
-import Button from 'react-bootstrap/Button';
 import { getLatestReleased } from '../../services/TmApi';
 import { useLocation } from 'react-router-dom';
 import { useQuery } from 'react-query';
 import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row'
 
-
-const Movies = () => {
+const Latest = () => {
   const { latest } = useLocation();
   const tumbnailPreImgUrl = "https://image.tmdb.org/t/p/w200";
-
   const { data, error, isError, isLoading } = useQuery('latest' , () =>
   getLatestReleased(latest));
 
@@ -20,22 +17,20 @@ const Movies = () => {
     <>
       <Container >
         <Row>
-        {isLoading && <p>Movies are laoding... </p>}
-        {isError && <p>An error occured: {error}</p>}
-        {data && data?.results.map((movie, i) => (
-          <Card style={{ width: '14rem' }} className=" m-2" key={i}>
-          <Card.Title className='text-center my-2 text-dark'>
-            <img src={tumbnailPreImgUrl + movie.poster_path} />
-        <span className="text-dark">{movie.original_title}</span>
-          </Card.Title>
-          
-        </Card>
-        ))}
-
-</Row>
+          {isLoading && <p>Movies are loading... </p>}
+          {isError && <p>An error occured: {error}</p>}
+          {data && data?.results.map((movie, i) => (
+            <Card style={{ width: '14rem' }} className=" m-2" key={i}>
+              <Card.Title className='text-center my-2 text-dark'>
+                <img src={tumbnailPreImgUrl + movie.poster_path} />
+                <span className="text-dark">{movie.original_title}</span>
+              </Card.Title>
+            </Card>
+          ))}
+        </Row>
       </Container>
     </>
   );
 };
 
-export default Movies;
+export default Latest;
