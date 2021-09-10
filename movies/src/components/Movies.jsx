7a505d7c6603fa25cea40ecc1f9b7c06e424fb3c
Mona Khorasani <mona.khorasani@medieinstitutet.se>
2021-09-10 09:06:19 +0200

    Implemented Movies component

---
+++ movies/src/components/Movies.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import Card from 'react-bootstrap/Card';
+import Button from 'react-bootstrap/Button';
+
+const Movies = () => {
+  return (
+    <>
+      <Card style={{ width: '18rem' }}>
+        <Card.Img variant='top' src='holder.js/100px180' />
+        <Card.Body>
+          <Card.Title>Movie Title</Card.Title>
+          <Button variant='primary'>More info</Button>
+        </Card.Body>
+      </Card>
+    </>
+  );
+};
+
+export default Movies;
