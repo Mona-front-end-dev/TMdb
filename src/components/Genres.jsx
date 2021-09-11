e6289ea88e88c2237b4cd254631c7746ef60c3c9
Mona Khorasani <mona.khorasani@medieinsitutet.se>
2021-09-11 12:21:33 +0200

    Refactor

--- src/components/Genres.jsx
+++ src/components/Genres.jsx
@@ -4,6 +4,8 @@ import { useLocation } from 'react-router-dom';
 import { useQuery } from 'react-query';
 import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+import { Link } from 'react-router-dom';
 
 
 const Genres = () => {
@@ -15,16 +17,18 @@ const Genres = () => {
   return (
     <>
       <Container >
-        
-          {isLoading && <p>Movies are laoding... </p>}
-          {isError && <p>An error occured: {error}</p>}           
-          {data && data?.genres.map((genre, i) => (
-            <Container>
-                <Row >
-                    {genre.name}
-                </Row> 
-            </Container>             
-          ))}
+        <Row className="m-2">
+            <Col md={4}>
+                {isLoading && <p>Movies are laoding... </p>}
+                {isError && <p>An error occured: {error}</p>}           
+                {data && data?.genres.map((genre, i) => (
+                    <Link key={genre.id} className="genre-link" to={`${genre.id}`}>{genre.name}</Link>         
+                ))}
+            </Col>
+            <Col md={8}>
+                jdhnljsjdklfs
+            </Col>
+        </Row>
       </Container>
     </>
   );
