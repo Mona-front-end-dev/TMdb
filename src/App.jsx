a6b940c4358072c4af8e2ba2b804f45ae0f739af
Mona Khorasani <mona.khorasani@medieinsitutet.se>
2021-09-11 12:20:40 +0200

    Refactor, added tabs

--- src/App.jsx
+++ src/App.jsx
@@ -2,19 +2,35 @@ import React from 'react';
 import { Route, Switch } from 'react-router-dom';
 import Navigation from './pages/partials/Navigation';
 import HomePage from './pages/HomePage';
-
+import Latest from './components/Movies/Latest';
+import Tabs from './components/Tabs';
 import './App.scss';
+import TopListed from './components/Movies/TopListed';
+import Genres from './components/Genres';
+import Popular from './components/Movies/Popular';
 
 function App() {
   return (
     <>
       <Navigation />
-        <Switch>
-          <Route exact path='/'>
-            <HomePage />
-          </Route>
-        </Switch>
-      
+      <Tabs />
+      <Switch>
+        <Route exact path='/'>
+          <HomePage />
+        </Route>
+        <Route path='/latest'>
+          <Latest />
+        </Route>
+        <Route path='/top'>
+          <TopListed />
+        </Route>
+        <Route path='/genres/:id'>
+          <Genres />
+        </Route>
+        <Route path='/popular'>
+          <Popular />
+        </Route>
+      </Switch>
     </>
   );
 }
