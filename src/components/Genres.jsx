d2f263251a4d71afbab1ec74a41b4f837a6a804c
Mona Khorasani <mona.khorasani@medieinsitutet.se>
2021-09-14 18:57:37 +0200

    Refactored GenresComponent

--- src/components/Genres.jsx
+++ src/components/Genres.jsx
@@ -1,33 +1,38 @@
 import React from 'react';
 import { getGenres } from '../services/TmApi';
-import { useLocation } from 'react-router-dom';
 import { useQuery } from 'react-query';
 import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
 import { Link } from 'react-router-dom';
-
+import Genre from '../components/Genre';
 
 const Genres = () => {
-  const { genres } = useLocation();
-  const { data, error, isError, isLoading } = useQuery('genres' , () =>
-  getGenres(genres));
-
+  const { data, error, isError, isLoading } = useQuery('genres', () =>
+    getGenres()
+  );
 
   return (
     <>
-      <Container >
-        <Row className="m-2">
-            <Col md={4}>
-                {isLoading && <p>Movies are laoding... </p>}
-                {isError && <p>An error occured: {error}</p>}           
-                {data && data?.genres.map((genre, i) => (
-                    <Link key={genre.id} className="genre-link" to={`${genre.id}`}>{genre.name}</Link>         
-                ))}
-            </Col>
-            <Col md={8}>
-                jdhnljsjdklfs
-            </Col>
+      <Container>
+        <Row className='m-2'>
+          <Col md={4}>
+            {isLoading && <p>Movies are laoding... </p>}
+            {isError && <p>An error occured: {error}</p>}
+            {data &&
+              data?.genres.map((genre, i) => (
+                <Link
+                  key={genre.id}
+                  className='genre-link'
+                  to={`/genres/${genre.id}`}
+                >
+                  {genre.name}
+                </Link>
+              ))}
+          </Col>
+          <Col md={8}>
+            <Genre />
+          </Col>
         </Row>
       </Container>
     </>
