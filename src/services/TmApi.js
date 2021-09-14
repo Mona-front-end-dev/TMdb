7348337d66ba93c3bdd6b3f6454ba2dc4b84bfcd
Mona Khorasani <mona.khorasani@medieinsitutet.se>
2021-09-14 18:54:11 +0200

    Created endpoint for getMoviesByGenreId

--- src/services/TmApi.js
+++ src/services/TmApi.js
@@ -3,42 +3,65 @@
  * https://api.themoviedb.org/
  */
 
- import axios from 'axios';
+import axios from 'axios';
 
- const baseUrl = "https://api.themoviedb.org/3/";
- 
- // returns a promise
- export const get = async (endpoint) => {
-   let respons = await axios.get(baseUrl + endpoint);
-   return respons;
- };
- 
- //create Endpoint to get latest released movies
- export const getLatestReleased = async () => {
-   const endpoint = 'discover/movie?api_key=e11ef120fb831c1ff10324b71ce6c686&language=en-US&page=1&region=US&sort_by=release_date.desc&release_date.lte=2021-09-08'; 
-   const data = await get(endpoint);
-   return data.data;
- };
+const baseUrl = 'https://api.themoviedb.org/3/';
 
- //create Endpoint to get the most popular movies
- export const getMostPopular = async () => {
-  const endpoint = 'discover/movie?api_key=e11ef120fb831c1ff10324b71ce6c686&language=en-US&page=1&region=US&primary_release_date.lte=2021-09-08'; 
+// returns a promise
+export const get = async (endpoint) => {
+  let respons = await axios.get(baseUrl + endpoint);
+  return respons;
+};
+
+//create Endpoint to get latest released movies
+export const getLatestReleased = async () => {
+  const endpoint =
+    'discover/movie?api_key=e11ef120fb831c1ff10324b71ce6c686&language=en-US&page=1&region=US&sort_by=release_date.desc&release_date.lte=2021-09-08';
+  const data = await get(endpoint);
+  return data.data;
+};
+
+//create Endpoint to get the most popular movies
+export const getMostPopular = async () => {
+  const endpoint =
+    'discover/movie?api_key=e11ef120fb831c1ff10324b71ce6c686&language=en-US&page=1&region=US&primary_release_date.lte=2021-09-08';
   const data = await get(endpoint);
   return data.data;
 };
 
 //create Endpoint to get top-listed movies
 export const getTopListed = async () => {
-  const endpoint = 'movie/top_rated?api_key=e11ef120fb831c1ff10324b71ce6c686&language=en-US&page=1&region=US&sort_by=vote-avarage.desc'; 
+  const endpoint =
+    'movie/top_rated?api_key=e11ef120fb831c1ff10324b71ce6c686&language=en-US&page=1&region=US&sort_by=vote-avarage.desc';
   const data = await get(endpoint);
   return data.data;
 };
 
 //create Endpoint to get genres
 export const getGenres = async () => {
-  const endpoint = 'genre/movie/list?api_key=e11ef120fb831c1ff10324b71ce6c686&language=en-US'; 
+  const endpoint =
+    'genre/movie/list?api_key=e11ef120fb831c1ff10324b71ce6c686&language=en-US';
   const data = await get(endpoint);
   return data.data;
 };
- 
 
+/**
+ * @param {number} id Genre ID
+ * @returns Promis
+ */
+//create Endpoint to get movies by genre ID
+export const getMoviesByGenreId = async (id) => {
+  const endpoint =
+    'discover/movie?api_key=e11ef120fb831c1ff10324b71ce6c686&language=en-US&page=1&region=US&primary_release_date.lte=2021-09-08&with_genres=' +
+    id;
+  const data = await get(endpoint);
+  return data.data;
+};
+
+export default {
+  getLatestReleased,
+  getMostPopular,
+  getTopListed,
+  getGenres,
+  getMoviesByGenreId,
+};
