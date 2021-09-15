22a2974255e96a5da002e8d3762d3830298bd73b
Mona Khorasani <mona.khorasani@medieinsitutet.se>
2021-09-15 15:10:45 +0200

    Corrected rout link and changed actor to person

--- src/components/cards/CharacterCard.jsx
+++ src/components/cards/CharacterCard.jsx
@@ -8,21 +8,21 @@ const CharacterCard = ({ data }) => {
   return (
     <>
       <h2>Charachters:</h2>
-      {data?.credits.cast.map((actor, i) => (
+      {data?.credits.cast.map((person, i) => (
         <Link
           variant='dark'
           className='d-flex'
-          to={`/credits/character/${actor.id}`}
+          to={`/people/${person.id}`}
         >
           <Card>
             <Card.Img
               variant='top'
-              src={tumbnailPreImgUrl + actor.profile_path}
+              src={tumbnailPreImgUrl + person.profile_path}
               className='img-fluid'
             />
             <Card.Body className='text-dark border'>
-              <Card.Title>{actor.name}</Card.Title>
-              <Card.Text className='text-dark'>{actor.character}</Card.Text>
+              <Card.Title>{person.name}</Card.Title>
+              <Card.Text className='text-dark'>{person.character}</Card.Text>
             </Card.Body>
           </Card>
         </Link>
