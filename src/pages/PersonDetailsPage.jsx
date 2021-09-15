06f20a6bafdc6512212230f5517eb871e7e9dcfe
Mona Khorasani <mona.khorasani@medieinsitutet.se>
2021-09-15 18:42:03 +0200

    Implemented PersonDetailsPageComponent

---
+++ src/pages/PersonDetailsPage.jsx
@@ -0,0 +1,52 @@
+import React from 'react';
+import { useQuery } from 'react-query';
+import Card from 'react-bootstrap/Card';
+import { useParams } from 'react-router-dom';
+import { getPersonsDetailsByPersonId } from '../services/TmApi';
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+import Image from 'react-bootstrap/Image';
+
+const PersonDetailsPage = () => {
+  const { id } = useParams();
+  const tumbnailPreImgUrl = 'https://image.tmdb.org/t/p/w200';
+
+  const { data, error, isError, isLoading } = useQuery(
+    ['personDetails', id],
+    () => getPersonsDetailsByPersonId(id)
+  );
+
+  return (
+    <>
+      {isLoading && <p>Details are loading... </p>}
+      {isError && <p>An error occured: {error}</p>}
+      {data && (
+        <Card className='shadow'>
+          <Row className=' mt-5 mx-3'>
+            <Col>
+              <Image src={tumbnailPreImgUrl + data.profile_path} rounded />
+              <Card.Body className='text-dark'>
+                <Card.Text className='text-dark'>
+                  Born on {data.birthday}
+                </Card.Text>
+                <Card.Text className='text-dark'>
+                  Born att {data.place_of_birth}
+                </Card.Text>
+                <Card.Text className='text-dark'>
+                  Known for {data.known_for_department} department
+                </Card.Text>
+              </Card.Body>
+            </Col>
+            <Col sm={8}>
+              <Card.Title>{data.name}</Card.Title>
+              <Card.Text>{data.biography}</Card.Text>
+            </Col>
+          </Row>
+        </Card>
+      )}
+    </>
+  );
+};
+
+export default PersonDetailsPage;
