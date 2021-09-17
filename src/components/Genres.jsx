7f9d58eb96016d66b4d87bb54345127938388041
Mona Khorasani <mona.khorasani@medieinsitutet.se>
2021-09-17 10:17:35 +0200

    cleaning

--- src/components/Genres.jsx
+++ src/components/Genres.jsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import { getGenres } from '../services/TmApi';
 import { useQuery } from 'react-query';
-import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
 import { Link } from 'react-router-dom';
@@ -14,27 +13,25 @@ const Genres = () => {
 
   return (
     <>
-     
-        <Row className='m-2'>
-          <Col md={4}>
-            {isLoading && <p>Movies are loading... </p>}
-            {isError && <p>An error occured: {error}</p>}
-            {data &&
-              data?.genres.map((genre, i) => (
-                <Link
-                  key={genre.id}
-                  className='genre-link'
-                  to={`/genres/${genre.id}`}
-                >
-                  {genre.name}
-                </Link>
-              ))}
-          </Col>
-          <Col md={8}>
-            <Genre />
-          </Col>
-        </Row>
-     
+      <Row className='m-2'>
+        <Col md={4}>
+          {isLoading && <p>Movies are loading... </p>}
+          {isError && <p>An error occured: {error}</p>}
+          {data &&
+            data?.genres.map((genre, i) => (
+              <Link
+                key={genre.id}
+                className='genre-link'
+                to={`/genres/${genre.id}`}
+              >
+                {genre.name}
+              </Link>
+            ))}
+        </Col>
+        <Col md={8}>
+          <Genre />
+        </Col>
+      </Row>
     </>
   );
 };
